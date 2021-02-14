ae734e488bf04678289d522c103049740c8c5589
Amine Moulay <aminepa8@gmail.com>
2021-02-14 21:31:40 +0100

    Added Unit test for employees api endpoint

---
+++ src/test/java/com/javaprojects/springboot/EmployeeControllerTest.java
@@ -0,0 +1,40 @@
+package com.javaprojects.springboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.javaprojects.springboot.controller.EmployeeController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+	@Autowired
+	private EmployeeController employeeController;
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(employeeController).isNotNull();
+	}
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnOkayHttpStatusAllEmployees() throws Exception {
+		this.mockMvc.perform(get("/api/v1/employees")).andDo(print()).andExpect(status().isOk());
+	}
+	
+
+}
