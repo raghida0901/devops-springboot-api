afad2640ffb47dc5866af4f4f25922df5b7bac66
Amine Moulay <aminepa8@gmail.com>
2021-02-14 15:55:47 +0100

    Creating List Employee REST API

---
+++ src/main/java/com/javaprojects/springboot/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.javaprojects.springboot.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaprojects.springboot.model.Employee;
+import com.javaprojects.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository  employeeRepository;
+	
+	
+	// get all employees
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+}
