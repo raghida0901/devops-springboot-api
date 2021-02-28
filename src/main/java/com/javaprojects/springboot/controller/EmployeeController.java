ac458118f8e194b0199f5ba0e70ee31b969c38fc
chamarh <63731183+chamarh@users.noreply.github.com>
2021-02-28 11:43:26 +0100

    Update EmployeeController.java
    add "Update Task Functionality"

--- src/main/java/com/javaprojects/springboot/controller/EmployeeController.java
+++ src/main/java/com/javaprojects/springboot/controller/EmployeeController.java
@@ -2,13 +2,17 @@ package com.javaprojects.springboot.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javaprojects.springboot.exception.ResourceNotFoundException;
 import com.javaprojects.springboot.model.Employee;
 import com.javaprojects.springboot.repository.EmployeeRepository;
 
@@ -34,4 +38,17 @@ public class EmployeeController {
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
+	
+	// update employee rest api 
+	@PutMapping("/employee/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailID(employeeDetails.getEmailID());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+		}
 }
