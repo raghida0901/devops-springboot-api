73383dbd4a852722646ce71772f1c4e1801dabd0
chamarh <63731183+chamarh@users.noreply.github.com>
2021-02-28 11:57:49 +0100

    Update EmployeeController.java
    add " delete task functionality"

--- src/main/java/com/javaprojects/springboot/controller/EmployeeController.java
+++ src/main/java/com/javaprojects/springboot/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.javaprojects.springboot.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,4 +56,18 @@ public class EmployeeController {
 		Employee updatedEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updatedEmployee);
 		}
+	
+	// delete employee rest api 
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+	
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+		
+		}
+
 }
